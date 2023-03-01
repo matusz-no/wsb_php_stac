2bebd2a4410f7076a471988ef58697cdf96af362
matusz-no <126659155+matusz-no@users.noreply.github.com>
2023-03-01 11:02:40 +0100

    Add files via upload

---
+++ projekt.php
@@ -0,0 +1,11 @@
+<?php
+$firstName = "Janusz";
+$lastName = "Nowak";
+echo "Imiê i nazwisko: $firstName $lastName<br>";
+echo 'Imiê i nazwisko: $firstName $lastName<br>';
+
+echo <<< DATA
+<hr>
+Imiê: $firstName<br>
+Nazwisko: $lastName<br>
+DATA;
